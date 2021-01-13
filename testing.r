497de00f3d2ff8fcab019274caf87e1719814a36
Muhammad shahid <68581672+Shahid-313@users.noreply.github.com>
2021-01-12 20:12:43 -0800

    few

--- testing.r
+++ testing.r
@@ -7,3 +7,12 @@ version = "Rstudio - in writing"
 print(name)
 print("These lines has been written in Rstudio")
 
+#vectors
+vec1 <- c(1,2,3)
+vec2 <- c("a","b","c")
+vec3 <- c(T,F,T)
+
+mix1 <- c(1,T,2,F)
+mix2 <- c(1,"a",2,"b")
+mix3 <- c(1,"a",T)
+
